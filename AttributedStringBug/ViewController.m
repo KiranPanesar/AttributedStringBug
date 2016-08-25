49b1ee970c15f5eba5e7bb821e19e526e8a73256
Kiran Panesar <kiran@mobilexlabs.com>
2016-08-25 17:59:30 -0500

    Committing files to demonstrated bug in TextKit

--- AttributedStringBug/ViewController.m
+++ AttributedStringBug/ViewController.m
@@ -17,6 +17,34 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.view.backgroundColor = [UIColor purpleColor];
+    
+    // Create a label
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(20.0f, 20.0f, self.view.frame.size.width - 40.0f, 30.0f)];
+    
+    // Create an NSTextAttachment with our image. This WILL NOT be green due to the bug.
+    NSTextAttachment *attachment = [[NSTextAttachment alloc] init];
+    attachment.image = [UIImage imageNamed:@"video_icon"];
+    
+    // Create a base NSAttributedString starting with the `attachment`
+    NSMutableAttributedString *bodyText = [[NSMutableAttributedString alloc] initWithAttributedString:[NSAttributedString attributedStringWithAttachment:attachment]];
+    
+    // Append the text that will be coloured green
+    [bodyText appendAttributedString:[[NSAttributedString alloc] initWithString:@"This is green"]];
+    
+    // This attachment will be appended at the end of the string and WILL be green.
+    NSTextAttachment *greenAttachment = [[NSTextAttachment alloc] init];
+    greenAttachment.image = [UIImage imageNamed:@"video_icon"];
+    [bodyText appendAttributedString:[NSAttributedString attributedStringWithAttachment:greenAttachment]];
+    
+    // Apply a foreground color attribute to the whole string
+    [bodyText addAttribute:NSForegroundColorAttributeName value:[UIColor greenColor] range:NSMakeRange(0, bodyText.length)];
+    
+    // Set the attributed text of the label
+    label.attributedText = bodyText;
+    
+    [self.view addSubview:label];
 }
 
 - (void)didReceiveMemoryWarning {
